b8bc5283c7b019546f981d4e1ae0f2fb4b3f1158
David <David.Piruzashvili@gmail.com>
2022-10-28 14:17:31 -0400

    final_commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,22 +1,31 @@
 const hamburger = document.querySelector(".hamburger"),
-   //   menu = document.querySelector('.menu'),
+     container = document.querySelector('.container'),
    menu = document.getElementById("block"),
    main = document.getElementById("main"),
    overlay = document.querySelector(".menu__overlay"),
    close = document.querySelector(".menu__close");
 
+
+
 hamburger.addEventListener("click", () => {
    //   menu.classList.add('active');
-   menu.style.width = "345px";
-   main.style.marginLeft = "180px";
+   document.body.style.overflow = 'hidden';
+   menu.style.width = "320px";
+   if(container.offsetWidth <= 540) {
+      main.style.marginLeft = "0px";
+   }else {
+      main.style.marginLeft = "160px";
+   }
    overlay.style.cssText = `
                background: rgba(0,0,0,0.4);
                z-index: 1; 
                `;
+  
 });
 
 close.addEventListener("click", () => {
    //   menu.classList.remove('active');
+   document.body.style.overflow = '';
    menu.style.width = "0";
    main.style.marginLeft = "0";
    overlay.style.cssText = `
@@ -28,6 +37,8 @@ close.addEventListener("click", () => {
 
 const el = document.querySelectorAll(".skills__progressbar-bar"),
    elText = document.querySelectorAll('.skills__progressbar-rate');
+
+const pageup = document.getElementById('pageup');
 
 
 function fillOutOnScroll(el, elText, index = 0) {
@@ -54,7 +65,18 @@ function actionOnscroll() {
       fillOutOnScroll(el, elText);
       window.removeEventListener('scroll', actionOnscroll);
    }
-
+   
 }
 
 window.addEventListener('scroll', actionOnscroll);
+
+
+function pageUp() {
+   if(document.documentElement.scrollTop > 1600) {
+      pageup.classList.add('pageup__show');
+   }else {
+      pageup.classList.remove('pageup__show');
+   }
+}
+
+window.addEventListener('scroll', pageUp);
